50ccf9a3978be53a2b2a99cb91b6de00c41d5e0d
mika1407 <koneautomaatio@hotmail.com>
2020-02-17 11:26:45 +0200

    last push

---
+++ app.js
@@ -0,0 +1,10 @@
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.3,
+  image1: "woman1.jpg",
+  image2: "woman2.jpg",
+  // speedIn: 1,
+  // speedOut: 1,
+
+  displacementImage: "./diss.png"
+});
